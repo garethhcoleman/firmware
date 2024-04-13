ff6c5a0c8a7a8ab670122c23b36c076865f2129c
Gareth Coleman <gareth@sublime.org>
2024-04-13 08:24:21 +0100

    trunk run

--- variants/unphone/variant.cpp
+++ variants/unphone/variant.cpp
@@ -3,13 +3,17 @@
 #include "unPhone.h"
 unPhone u = unPhone("meshtastic_unphone");
 
-void initVariant() {
-  u.begin();                    // initialise hardware etc.
-  u.store(u.buildTime);
-  u.printWakeupReason();        // what woke us up? (stored, not printed :|)
-  u.checkPowerSwitch();         // if power switch is off, shutdown
+void initVariant()
+{
+    u.begin(); // initialise hardware etc.
+    u.store(u.buildTime);
+    u.printWakeupReason(); // what woke us up? (stored, not printed :|)
+    u.checkPowerSwitch();  // if power switch is off, shutdown
 
-  for(int i = 0; i < 3; i++) {  // buzz a bit
-    u.vibe(true);  delay(150); u.vibe(false); delay(150);
-  }
+    for (int i = 0; i < 3; i++) { // buzz a bit
+        u.vibe(true);
+        delay(150);
+        u.vibe(false);
+        delay(150);
+    }
 }
